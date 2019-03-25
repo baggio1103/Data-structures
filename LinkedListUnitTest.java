7ed0c5eefdb4cd2364145ba0204c4d510077eb46
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-03-25 21:52:04 +0300

    Update LinkedListUnitTest.java

--- LinkedListUnitTest.java
+++ LinkedListUnitTest.java
@@ -7,17 +7,11 @@ public class LinkedListTest {
     public void remove() {
         LinkedList list = new LinkedList();
         list.addInTail(new Node(3));
-//        System.out.println(list.count());
-        int count = 0;
+//      System.out.println(list.count());
         list.remove(3);
-        Node actualHead = null;
-        Node actualTail = null;
-        int expectedCount = list.count();
-        Node head = list.head;
-        Node tail = list.tail;
-//        System.out.println(list.count());
-        assertEquals(count,expectedCount);
-        assertEquals(actualHead,head);
-        assertEquals(actualTail,tail);
+//      System.out.println(list.count());
+        assertEquals(0,list.count());
+        assertEquals(null,list.head);
+        assertEquals(null,list.tail);  
     }
 }
