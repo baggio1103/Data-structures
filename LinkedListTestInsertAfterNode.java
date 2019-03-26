9e37f9a30cc147ed9de900499e52cea3c5aa0a1a
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-03-26 18:36:10 +0300

    Update LinkedListTestInsertAfterNode.java

--- LinkedListTestInsertAfterNode.java
+++ LinkedListTestInsertAfterNode.java
@@ -5,15 +5,15 @@ import static org.junit.Assert.*;
 public class LinkedListTest {
     @Test
     public void insertAfter() {
-    LinkedList list = new LinkedList();
-        list.addInTail(new Node(1));
-        list.addInTail(new Node(2));
-        list.addInTail(new Node(3));
-        list.addInTail(new Node(5));
-        //list.printList();
-        list.insertAfter(new Node(3),new Node(4));
-        assertEquals(5,list.count());
-        //list.printList();
-
-  }
+  LinkedList list = new LinkedList();
+      list.addInTail(new Node(1));
+      list.addInTail(new Node(2));
+      list.addInTail(new Node(3));
+      list.addInTail(new Node(4));
+  //    list.printList();
+      list.insertAfter(new Node(4),new Node(5));
+      assertEquals(5,list.count());
+      assertEquals(5,list.tail.value);
+//      list.printList();
+    }
 }
