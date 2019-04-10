427d15fefdaf9deaad3770a0ebdae23e7bbea628
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-04-11 00:47:03 +0300

    Update DynArrayJUnitTest.java

--- DynArrayJUnitTest.java
+++ DynArrayJUnitTest.java
@@ -12,21 +12,31 @@ public class DynArrayTest {
         list.insert(77,0);
         assertEquals(1,list.count);
         assertEquals(16,list.capacity);
+        for (int i =0; i < 1125; i++){
+            list.append(i);
+        }
+        assertEquals(1126,list.count);
+        assertEquals(2048,list.capacity);
+        for (int i = list.count-1; i > 100; i--){
+            list.remove(i);
+        }
     }
 
     @Test
     public void remove() {
-        DynArray<Integer> list = new DynArray<>(Integer.class);
-        for (int i = 0; i <1047;i++){
-            list.append(i);
-        }
-//        System.out.println(list.count);
-//        System.out.println(list.capacity);
-       for (int i = list.count-1; i > 45;i--){
-           list.remove(i);
-       }
-       assertEquals(64,list.capacity);
-       assertEquals(46,list.count);
-        System.out.println(list.count);
+     DynArray<Integer> list = new DynArray<>(Integer.class);
+     for (int i = 0; i <1048; i++){
+         list.append(i);
+     }
+     list.remove(999);
+     assertEquals(2048,list.capacity);
+     for (int i = list.count-1; i >47; i--){
+         list.remove(i);
+     }
+     assertEquals(79,list.capacity);
+    for (int i =list.count-1; i > 8; i--){
+        list.remove(i);
     }
+        assertEquals(16,list.capacity);
+     }
 }
