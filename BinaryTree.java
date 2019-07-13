e7211f79d81666f33a261a5bf89fa88805c816f6
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-07-13 10:31:43 +0500

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,255 @@
+import java.io.*;
+import java.util.*;
+
+
+class BSTNode<T>
+{
+    public int NodeKey; // ключ узла
+    public T NodeValue; // значение в узле
+    public BSTNode<T> Parent; // родитель или null для корня
+    public BSTNode<T> LeftChild; // левый потомок
+    public BSTNode<T> RightChild; // правый потомок
+    public boolean haveOneChild; //true, if a node have a child
+    public boolean leftOrRightChild;
+    public boolean haveTwoChildren;
+
+    public BSTNode(int key, T val, BSTNode<T> parent)
+    {
+        NodeKey = key;
+        NodeValue = val;
+        Parent = parent;
+        LeftChild = null;
+        RightChild = null;
+    }
+}
+
+// промежуточный результат поиска
+class BSTFind<T>
+{
+    // null если не найден узел, и в дереве только один корень
+    public BSTNode<T> Node;
+
+    // true если узел найден
+    public boolean NodeHasKey;
+
+    // true, если родительскому узлу надо добавить новый левым
+    public boolean ToLeft;
+
+    public BSTFind() {
+        Node = null;
+    }
+}
+
+class BST<T>
+{
+    BSTNode<T> Root; // корень дерева, или null
+    public int count;
+
+    public BST(BSTNode<T> node)
+    {
+        Root = node;
+        count = 1;
+    }
+
+    public BSTFind<T> FindNodeByKey(int key) {
+        // ищем в дереве узел и сопутствующую информацию по ключу
+        BSTFind<T> bstFind = new BSTFind<>();
+        BSTNode<T> parentNode;// in case of null we have a parentNode to which a new node is added
+        BSTNode<T> node = Root;
+        if (node.NodeKey == key){
+            bstFind.Node = node;
+            bstFind.NodeHasKey = true;
+            bstFind.Node.Parent = null;
+            return bstFind;
+        }
+        else {
+            while (true) {
+                if (node.NodeKey == key) {
+                    bstFind.Node = node;
+                    bstFind.NodeHasKey = true;
+                    if (bstFind.Node.RightChild != null && bstFind.Node.LeftChild != null){
+                        bstFind.Node.haveTwoChildren = true;
+                    }
+                    return bstFind;
+                } else if (node.NodeKey < key) {
+                    bstFind.ToLeft = false;
+                    parentNode = node;
+                    node = node.RightChild;
+                } else {
+                    bstFind.ToLeft = true;
+                    parentNode = node;
+                    node = node.LeftChild;
+                }
+                if (node == null) {
+                    bstFind.Node = parentNode;
+                    return bstFind;
+                }
+            }
+        }
+    }
+
+    public boolean AddKeyValue(int key, T val) {
+        // добавляем ключ-значение в дерево
+        BSTFind<T> bstFind = FindNodeByKey(key);
+        BSTNode<T> nodeToAdd = new BSTNode<>(key, val, bstFind.Node);
+        if (!bstFind.NodeHasKey) {
+            count++;
+            bstFind.Node.haveOneChild = true;
+            if (bstFind.ToLeft) {
+                bstFind.Node.LeftChild = nodeToAdd;
+                nodeToAdd.leftOrRightChild = true; // i.e. added node is a left child
+                return true;
+            } else {
+                bstFind.Node.RightChild = nodeToAdd;
+                nodeToAdd.leftOrRightChild = false; // i.e. added node is a right child
+                return true;
+            }
+        } else {
+            return false; // если ключ уже есть
+        }
+    }
+
+    public BSTNode<T> FinMinMax(BSTNode<T> FromNode, boolean FindMax)
+    {
+        // ищем максимальное/минимальное в поддереве
+        if (FindMax){
+            while (true){
+                if (FromNode.RightChild == null){
+                    return FromNode;
+                }
+                else {
+                    FromNode = FromNode.RightChild;
+                }
+            }
+        }
+        else {
+            while (true){
+                if (FromNode.LeftChild == null){
+                    return FromNode;
+                }else {
+                    FromNode = FromNode.LeftChild;
+                }
+            }
+        }
+    }
+
+    public boolean DeleteNodeByKey(int key) {
+        // удаляем узел по ключу
+        BSTFind<T> bst = FindNodeByKey(key);
+        BSTNode<T> tempNode;
+        if (bst.NodeHasKey) {
+           count--;
+            //case1: a node has no node
+            if (!bst.Node.haveOneChild) {
+                if (bst.Node == Root){ // case when the only node is Root and it is to be deleted
+                    Root = null;
+                }else {
+                    if (bst.Node.leftOrRightChild) {
+                        bst.Node.Parent.LeftChild = null;
+                    } else {
+                        bst.Node.Parent.RightChild = null;
+                    }
+                }
+            }
+            //case2: a node has two children
+            else if (bst.Node.haveTwoChildren) {
+                if (bst.Node == Root) {
+                    tempNode = Root.RightChild;
+                    while (true){
+                        if (tempNode.LeftChild == null){
+                            BSTNode<T> node1 = Root.RightChild;
+                            tempNode.Parent = null;
+                            Root = tempNode;
+                            tempNode.RightChild = node1;
+                            break;
+                        }
+                        tempNode = tempNode.LeftChild;
+                    }
+                    tempNode = tempNode.RightChild;
+                    while (tempNode != null){
+                        if (tempNode.LeftChild == null && tempNode.RightChild != null){
+                            tempNode.Parent.LeftChild = tempNode.RightChild;
+                            tempNode.RightChild.Parent = tempNode.Parent;
+                        }else {
+                            tempNode.Parent.LeftChild = null;
+                        }
+                        tempNode = tempNode.LeftChild;
+                    }
+
+                } else {
+                    tempNode = bst.Node.RightChild;
+                    while (true) { // in this block I swapped the node to be deleted by the smallest node in the right subtree
+                        if (tempNode.LeftChild == null) {
+                            if (bst.Node.leftOrRightChild) {
+                                bst.Node.Parent.LeftChild = tempNode;
+                                tempNode.Parent = bst.Node.Parent;
+                            } else {
+                                bst.Node.Parent.RightChild = tempNode;
+                                tempNode.Parent = bst.Node.Parent;
+                            }
+                            break;
+                        }
+                        tempNode = tempNode.LeftChild;
+                    }
+                    tempNode.RightChild = bst.Node.RightChild;
+                    while (tempNode != null) { // here it is needed to remove the node to be deleted after swapping the positions
+                        if (tempNode.LeftChild == null && tempNode.RightChild != null) {
+                            tempNode.Parent.LeftChild = tempNode.RightChild;
+                            tempNode.RightChild.Parent = tempNode.Parent;
+                        } else {
+                            tempNode.Parent.LeftChild = null;
+                        }
+                        tempNode = tempNode.LeftChild;
+                    }
+                }
+            }
+
+            //case3: a node has one node
+            else {
+                if (bst.Node == Root){
+                    if (Root.RightChild == null){
+                        tempNode = Root.LeftChild;
+                        tempNode.Parent = null;
+                        Root = tempNode;
+                    }else {
+                        tempNode = Root.RightChild;
+                        tempNode.Parent = null;
+                        Root = tempNode;
+                    }
+
+                }else {
+                    if (bst.Node.LeftChild == null) {
+                        tempNode = bst.Node.RightChild;
+                    } else {
+                        tempNode = bst.Node.LeftChild;
+                    }
+                    if (bst.Node.leftOrRightChild) {
+                        bst.Node.Parent.LeftChild = tempNode;
+                        tempNode.Parent = bst.Node.Parent;
+                    } else {
+                        bst.Node.Parent.RightChild = tempNode;
+                        tempNode.Parent = bst.Node.Parent;
+                    }
+                }
+            }
+            return true;
+        }
+        return false; // если узел не найден
+    }
+
+    public void printTree(BSTNode<T> root){
+        if (root == null){
+            return;
+        }
+        System.out.print(root.NodeKey + " ");
+        printTree(root.LeftChild);
+        printTree(root.RightChild);
+    }
+
+
+    public int Count()
+    {
+        return count; // количество узлов в дереве
+    }
+    
+}
