91eb73ef308f71018953d9a7164b8f2cfb6f2112
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2019-07-27 00:28:07 +0500

    Create BalancedBinaryTree.java

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class AlgorithmsDataStructures2 {
+
+    public static int[] GenerateBBSTArray(int[] a) {
+        int[] tree = new int[a.length];
+        arraySort(a);
+        int index = 0;
+        int start = 0;
+        int end = a.length-1;
+        func(a,tree,start,end, index);
+        return tree;
+    }
+
+    public static void arraySort(int[] array){
+        for (int i = array.length-1; i >= 0; i--){
+            for (int j = 0; j < i; j++){
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+    
+    public static void func(int[] array, int[] finArr, int start, int end, int index){
+       if (start > end){
+           return;
+       }
+       int mid = (start+end)/2;
+       finArr[index] = array[mid];
+       func(array, finArr, start, mid-1, 2*index+1);
+       func(array, finArr,mid+1, end, 2*index+2);
+
+    }
+
+}
