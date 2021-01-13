1da21616eab9393b063e27814e8f3f9399b49918
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2021-01-13 15:28:21 +0300

    Update SimpleGraph.java

--- SimpleGraph.java
+++ SimpleGraph.java
@@ -1,67 +1,6 @@
 import java.util.*;
 import java.util.Stack;
 
-class Queue<T> {
-    public ArrayList list;
-
-    public Queue() {
-        // инициализация внутреннего хранилища очереди
-        list = new ArrayList();
-    }
-
-    public void enqueue(T item) {
-        // вставка в хвост
-        list.add(item);
-    }
-
-    public T queueLast(){
-        if (list.size() > 0){
-            return (T) list.get(list.size()-1);
-        }else {
-            return null;
-        }
-    }
-
-    public T dequeue() {
-        // выдача из головы
-        if (list.size() > 0) {
-            T type = (T) list.get(0);
-            list.remove(0);
-            return type;
-        } else
-            return null; // null если очередь пустая
-    }
-
-    public T peek(){
-        if (list.size() > 0){
-            T type = (T) list.get(0);
-            return type;
-        }else
-            return null;
-    }
-
-    public int size() {
-        return list.size();
-        // размер очереди
-    }
-
-    public Stack task4(Stack stack, Stack stack1) {
-        while (stack.size() > 0) {
-            stack1.push(stack.pop());
-        }
-        return stack1;
-    }
-
-    public Queue rotation(Queue queue, int cycle) {
-
-        for (int i = 0; i < cycle; i++) {
-            queue.enqueue(queue.dequeue());
-        }
-        return queue;
-    }
-
-}
-
 class Vertex
 {
     public int Value;
@@ -75,7 +14,7 @@ class Vertex
     }
 }
 
-class SimpleGraph {
+public class SimpleGraph {
     Vertex[] vertex;
     int[][] m_adjacency;
     int max_vertex;
@@ -317,6 +256,68 @@ class SimpleGraph {
             }
         }
         return list;
+    }
+
+}
+
+
+class Queue<T> {
+    public ArrayList list;
+
+    public Queue() {
+        // инициализация внутреннего хранилища очереди
+        list = new ArrayList();
+    }
+
+    public void enqueue(T item) {
+        // вставка в хвост
+        list.add(item);
+    }
+
+    public T queueLast(){
+        if (list.size() > 0){
+            return (T) list.get(list.size()-1);
+        }else {
+            return null;
+        }
+    }
+
+    public T dequeue() {
+        // выдача из головы
+        if (list.size() > 0) {
+            T type = (T) list.get(0);
+            list.remove(0);
+            return type;
+        } else
+            return null; // null если очередь пустая
+    }
+
+    public T peek(){
+        if (list.size() > 0){
+            T type = (T) list.get(0);
+            return type;
+        }else
+            return null;
+    }
+
+    public int size() {
+        return list.size();
+        // размер очереди
+    }
+
+    public Stack task4(Stack stack, Stack stack1) {
+        while (stack.size() > 0) {
+            stack1.push(stack.pop());
+        }
+        return stack1;
+    }
+
+    public Queue rotation(Queue queue, int cycle) {
+
+        for (int i = 0; i < cycle; i++) {
+            queue.enqueue(queue.dequeue());
+        }
+        return queue;
     }
 
 }
